2a38441e5f963ac7a25f2bd4fe0b9ff95ea01ded
unknown <amirimohammad117@gmail.com>
2023-10-22 15:14:16 +0330

    Functionality Added

--- main.py
+++ main.py
@@ -1,4 +1,89 @@
-from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button
+import json
+from random import choice, randint, shuffle
+from tkinter import *
+from tkinter import messagebox
+
+import pyperclip
+
+""" Generate Password """
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+               'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+               'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+               'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+               'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['$', '#', '!', '(', ')', '+', '&', '%', '*']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+
+    # ------ copy password to clipboard
+    pyperclip.copy(password)
+
+
+""" Save Password """
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure didn't left any field empty")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+""" Find Password """
+
+
+def find():
+    website = website_entry.get()
+    try:
+        with open("data.json", 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message="No Data file Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \n Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message="No details of entered website found")
+
 
 """ Window Setup """
 window = Tk()
@@ -33,13 +118,13 @@ password_entry = Entry(width=30)
 password_entry.grid(row=3, column=1)
 
 """ Button """
-search_button = Button(text="search", width=15)
+search_button = Button(text="search", width=15, command=find)
 search_button.grid(row=1, column=2)
 
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="add", width=42)
+add_button = Button(text="add", width=42, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
